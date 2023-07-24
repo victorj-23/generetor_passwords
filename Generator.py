51e42081a3520593371bc10b4e791f097e3faaec
Victor Marinho <vi.souza023@gmail.com>
2023-07-24 01:01:39 -0300

    modification for at least one character is special

--- Generator.py
+++ Generator.py
@@ -11,12 +11,14 @@ drawing_asc2 ='''
 print('\033[32m' + drawing_asc2 + '\033[0;0m')
 
 passwords = int(input('How many passwords do you want? '))
-leght = int(input('What length for the passwords? '))
+lenght = int(input('What length for the passwords? '))
 
 chars = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+special_chars = '.+-[]*~_@#:?'
 
 for _ in range(passwords):
     generate_passwords = ''
-    for _ in range(leght):
+    for _ in range(lenght - 1):
         generate_passwords += random.choice(chars)
+    generate_passwords += random.choice(special_chars)
     print(generate_passwords)
