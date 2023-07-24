af9486c6f0317732aa33bfbce37bb3ec7533e572
Victor Marinho <vi.souza023@gmail.com>
2023-07-24 15:12:50 -0300

    v3
    created two functions (give_char, giver_special_char) to simplify the main script and change the façade design

--- Generator.py
+++ Generator.py
@@ -1,24 +1,33 @@
-import random
+def give_char():
+    import random
+    chars = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return random.choice(chars)
+
+def give_special_char():
+    import random
+    special_chars = '.+-[]*~_@#:?'
+    return random.choice(special_chars)
 
 drawing_asc2 =''' 
-  _____                                    _        _____                           _             
- |  __ \                                  | |      / ____|                         | |            
- | |__) |_ _ ___ _____      _____  _ __ __| |___  | |  __  ___ _ __   ___ _ __ __ _| |_ ___  _ __ 
- |  ___/ _` / __/ __\ \ /\ / / _ \| '__/ _` / __| | | |_ |/ _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|
- | |  | (_| \__ \__ \\ V  V / (_) | | | (_| \__ \ | |__| |  __/ | | |  __/ | | (_| | || (_) | |   
- |_|   \__,_|___/___/ \_/\_/ \___/|_|  \__,_|___/  \_____|\___|_| |_|\___|_|  \__,_|\__\___/|_|                                                                                                  
+  ___                              _       ___                       _           
+ | _ \__ _ _______ __ _____ _ _ __| |___  / __|___ _ _  ___ _ _ __ _| |_ ___ _ _ 
+ |  _/ _` (_-<_-< V  V / _ \ '_/ _` (_-< | (_ / -_) ' \/ -_) '_/ _` |  _/ _ \ '_|
+ |_| \__,_/__/__/\_/\_/\___/_| \__,_/__/  \___\___|_||_\___|_| \__,_|\__\___/_|  
+                                                                                                                                                                                                                                                                                                
 '''
 print('\033[32m' + drawing_asc2 + '\033[0;0m')
 
-passwords = int(input('How many passwords do you want? '))
-lenght = int(input('What length for the passwords? '))
-
-chars = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-special_chars = '.+-[]*~_@#:?'
+while True:
+    try:
+        passwords = int(input('How many passwords do you want? '))
+        lenght = int(input('What length for the passwords? '))
+        break
+    except ValueError:
+        print('\033[31m' + 'ERROR: Use integers numbers' + '\033[0;0m')
 
 for _ in range(passwords):
     generate_passwords = ''
     for _ in range(lenght - 1):
-        generate_passwords += random.choice(chars)
-    generate_passwords += random.choice(special_chars)
+        generate_passwords += give_char()
+    generate_passwords += give_special_char()
     print(generate_passwords)
