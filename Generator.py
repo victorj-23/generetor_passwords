03d7f47f0e3141bf6f2075bf51c4bb353b112f65
victorj-23 <vi.souza023@gmail.com>
2023-08-10 13:12:38 -0300

    v4
    add functions for aesthetics, shortening the main code and some spelling changes for better code reading

--- Generator.py
+++ Generator.py
@@ -1,42 +1,35 @@
 from utilities import *
 
-drawing_asc2 =''' 
-  ___                              _       ___                       _           
- | _ \__ _ _______ __ _____ _ _ __| |___  / __|___ _ _  ___ _ _ __ _| |_ ___ _ _ 
- |  _/ _` (_-<_-< V  V / _ \ '_/ _` (_-< | (_ / -_) ' \/ -_) '_/ _` |  _/ _ \ '_|
- |_| \__,_/__/__/\_/\_/\___/_| \__,_/__/  \___\___|_||_\___|_| \__,_|\__\___/_|  
-                                                                                                                                                                                                                                                                                                
-'''
-print('\033[32m' + drawing_asc2 + '\033[0;0m')
-
+facade()
 
 # Data entry and some checks take place
 while True:
     try:
-        passwords = int(input('How many passwords do you want? '))
+        amount_passwords = int(input('How many passwords do you want? '))
         lenght = int(input('''What lenght for the passwords \033[31m(Min = 4)\033[0;0m? '''))
-        if passwords < 1  or lenght < 1:
-            print('\033[31m' + 'ERROR: Use positives numbers' + '\033[0;0m')
+        if amount_passwords < 1  or lenght < 1:
+            print(red_txt('ERROR: Use positives numbers'))
             continue
         if lenght < 4:
-            print('\033[31m' + 'ERROR: lenght below 4 is not accepted ' + '\033[0;0m')
+            print(red_txt('ERROR: lenght below 4 is not accepted'))
             continue
         if lenght < 8:
-            print('\033[32m' + 'Tip: use passwords with at least 8 characters' + '\033[0;0m')
+            print(green_txt('Tip: use passwords with at least 8 characters'))
         break
     except ValueError:
-        print('\033[31m' + 'ERROR: Use integers numbers' + '\033[0;0m')
+        print(red_txt('ERROR: Use integers numbers'))
 
 list_passwords =[]
 
 # Generator working
-while len(list_passwords) < passwords:
+while len(list_passwords) < amount_passwords:
     genereted_password = ''
     for _ in range(lenght - 1):
         genereted_password += give_char()
     genereted_password += give_special_char()
 
-    if check_password(genereted_password): # The password needs to meet some requirements
+    # The password needs to meet some requirements
+    if check_password(genereted_password): 
         list_passwords.append(genereted_password)
 
 for passwords in list_passwords:
--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,5 +1,6 @@
 from utilities.chars import *
 from utilities.passwords_validator import check_password
 from utilities.save import save_password
+from utilities.asthetic import *
 
-__add__ = ['give_char', 'give_special_char', 'check_password', 'save_password']
+__add__ = ['give_char', 'give_special_char', 'check_password', 'save_password', 'green_txt', 'red_txt', 'facade']
